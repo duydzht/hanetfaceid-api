d94723fb0783f79ba04d82bb68d91dd6bfebbb8b
Duy Hoàng <hoangduy241199@gmail.com>
2024-06-10 11:52:15 +0700

    [UPDATE]

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "postgres.dtcsolution.vn"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "db_hanet_faceid"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "123"),
+      password: env("DATABASE_PASSWORD", "KcrFZRvIl2AI4spYq27sI3PooPlllYf6GYY8PNfGsTeBHUBRI8RBqojbbuuG25gX"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
