70b132f39f62dc4d7791fcd55947216b8de8e274
Duy Hoàng <hoangduy241199@gmail.com>
2024-06-10 11:33:18 +0700

    [UPDATE]

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "hanetfaceid_tq"),
-      user: env("DATABASE_USERNAME", "hanetfaceid_tq"),
-      password: env("DATABASE_PASSWORD", "7y6c4a0k"),
+      database: env("DATABASE_NAME", "db_hanet_faceid"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "123"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
